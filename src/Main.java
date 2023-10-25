7138d4c5bcaef464db85029184697a7bbaa4a02c
Lương Chung Hội <87046628+lchryu@users.noreply.github.com>
2023-10-25 12:22:28 +0700

    1.Nhập xuất và chuyển đổi định dạng dữ liệu

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) throws Exception {
         MyKnowledgeModel model = new MyKnowledgeModel("D:\\Programs\\Weka-3-9-6\\data\\iris.arff");
         System.out.println(model.toString());
+
+        model.saveData("D:\\data_weka\\iris.arff");
+        model.saveData2CSV("D:\\data_weka\\iris.csv");
     }
 }
--- src/MyKnowledgeModel.java
+++ src/MyKnowledgeModel.java
@@ -1,5 +1,10 @@
 import weka.core.Instances;
+import weka.core.converters.ArffSaver;
+import weka.core.converters.CSVSaver;
 import weka.core.converters.ConverterUtils.DataSource;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MyKnowledgeModel {
     DataSource source;
@@ -13,7 +18,20 @@ public class MyKnowledgeModel {
         this.source = new DataSource(filename);
         this.dataset = source.getDataSet();
     }
-
+    public void saveData(String filename) throws IOException {
+        ArffSaver outData = new ArffSaver();
+        outData.setInstances(this.dataset);
+        outData.setFile(new File(filename));
+        outData.writeBatch();
+        System.out.println("Saving finished");
+    }
+    public void saveData2CSV(String filename) throws IOException {
+        CSVSaver outData = new CSVSaver();
+        outData.setFile(new File(filename));
+        outData.setInstances(this.dataset);
+        outData.writeBatch();
+        System.out.println("Converted");
+    }
     @Override
     public String toString() {
         return dataset.toSummaryString();
